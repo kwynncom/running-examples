accff3b9d0bf3cc7e3c7d91702449334cd6332bd
kwynncom <buessresume@gmail.com>
2022-01-18 02:06:30 -0500

    Nu as a service

---
+++ nu/installService.sh
@@ -0,0 +1,15 @@
+# I might miss something, but should give you the idea
+# assuming you are in this directory with the .sh
+# assumes /opt/misc exists and you have permssion
+# there is no need for root / sudo if not shown just below
+ln ./runnu.sh /opt/misc/
+# next command not necessary if using service, but might as well have it on hand.
+ln ./runNuAsNobody.sh /opt/misc/
+sudo ln ./nu.service /etc/systemd/system/
+# if you change the file of the service above
+sudo systemctl daemon-reload
+sudo systemctl start nu
+sudo systemctl status nu
+# might take 5 - 10 seconds before it responds to HTTP
+# start on reboot
+sudo systemctl enable nu
--- nu/runNuAsNobody.sh
+++ nu/runNuAsNobody.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/bash
 echo v - none script - 2022-01-14 00:52
 sudo -u nobody -g nogroup /opt/misc/runnu.sh
-
